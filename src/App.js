a3446ef474b84905aa5338e05ce0bd3cfbf33812
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-18 18:22:18 +0100

    App.css adding the Neumorphism style to the Search Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import react from "react";
+import React, { useState, useEffect } from "react";
+import './App.css'
+// import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=696896f0";
+
+const movie1 = {
+
+  "Title": "Batman Begins",
+  "Year": "2005",
+  "imdbID": "tt0372784",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+
+}
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+
+    setMovies(data.Search);
+  };
   return (
-    <h1>Hello World</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+      <div className="container">
+        <div className="movie">
+
+          <div>
+            <p> {movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : "https://via.placeholder.com/400"} alt={movie1.Title} />"
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
   );
 }
 
